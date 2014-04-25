11452833189a6cf682f0ef34e748125592444937
David Sveningsson <ext@sidvind.com>
2014-04-25 15:03:03 +0200

    Use width and height directly if no aspect ratio is passed.

--- src/cropper.js
+++ src/cropper.js
@@ -296,7 +296,7 @@
         setData: function(data) {
             var cropper = this.cropper,
                 dragger = this.dragger,
-                aspectRatio = this.defaults.aspectRatio || 1;
+                aspectRatio = this.defaults.aspectRatio;
 
             if (!this.active) {
                 return;
@@ -313,18 +313,23 @@
                     dragger.top = data.y1;
                 }
 
-                if ($.isNumeric(data.width) && data.width > 0 && data.width <= cropper.width) {
+                if ( !isNaN(aspectRatio) ){
+                    if ($.isNumeric(data.width) && data.width > 0 && data.width <= cropper.width) {
+                        dragger.width = data.width;
+                        dragger.height = dragger.width / aspectRatio;
+                    } else if ($.isNumeric(data.height) && data.height > 0 && data.height <= cropper.height) {
+                        dragger.height = data.height;
+                        dragger.width = dragger.height * aspectRatio;
+                    } else if ($.isNumeric(data.x2) && data.x2 > 0 && data.x2 <= cropper.width) {
+                        dragger.width = data.x2 - dragger.left;
+                        dragger.height = dragger.width / aspectRatio;
+                    } else if ($.isNumeric(data.y2) && data.y2 > 0 && data.y2 <= cropper.height) {
+                        dragger.height = data.y2 - dragger.top;
+                        dragger.width = dragger.height * aspectRatio;
+                    }
+                } else {
                     dragger.width = data.width;
-                    dragger.height = dragger.width / aspectRatio;
-                } else if ($.isNumeric(data.height) && data.height > 0 && data.height <= cropper.height) {
                     dragger.height = data.height;
-                    dragger.width = dragger.height * aspectRatio;
-                } else if ($.isNumeric(data.x2) && data.x2 > 0 && data.x2 <= cropper.width) {
-                    dragger.width = data.x2 - dragger.left;
-                    dragger.height = dragger.width / aspectRatio;
-                } else if ($.isNumeric(data.y2) && data.y2 > 0 && data.y2 <= cropper.height) {
-                    dragger.height = data.y2 - dragger.top;
-                    dragger.width = dragger.height * aspectRatio;
                 }
             } else {
                 dragger.left = cropper.width * 0.1;
