bd5747df6c16876f62a386afd1531a283a064f56
Fengyuan Chen <cfy.net@gmail.com>
2015-01-03 17:43:22 +0800

    Revert "Fix 144"
    This reverts commit e0c0a8843b5eeacf66d19c86f31cf3de9eb2e29b.

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 The MIT License (MIT)
 
-Copyright (c) 2014-2015 Fengyuan Chen and other contributors.
+Copyright (c) 2014 Fengyuan Chen and other contributors.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- dist/cropper.js
+++ dist/cropper.js
@@ -1,8 +1,8 @@
 /*!
- * Cropper v0.7.7
+ * Cropper v0.7.6
  * https://github.com/fengyuanchen/cropper
  *
- * Copyright 2014-2015 Fengyuan Chen
+ * Copyright 2014 Fengyuan Chen
  * Released under the MIT license
  */
 
@@ -487,7 +487,6 @@
           // If not set, use the original aspect ratio of the image.
           aspectRatio = defaults.aspectRatio || this.image.aspectRatio,
           ratio = this.image.ratio,
-          autoCropDragger = {},
           dragger;
 
       if (((cropper.height * aspectRatio) - cropper.width) >= 0) {
@@ -541,17 +540,15 @@
       dragger.minHeight = min(dragger.maxHeight, dragger.minHeight);
 
       // Center the dragger by default
-      autoCropDragger.height = dragger.height * defaults.autoCropArea;
-      autoCropDragger.width = dragger.width * defaults.autoCropArea;
-      autoCropDragger.left = (cropper.width - autoCropDragger.width) / 2;
-      autoCropDragger.top = (cropper.height - autoCropDragger.height) / 2;
+      dragger.height *= defaults.autoCropArea;
+      dragger.width *= defaults.autoCropArea;
+      dragger.left = (cropper.width - dragger.width) / 2;
+      dragger.top = (cropper.height - dragger.height) / 2;
+      dragger.oldLeft = dragger.left;
+      dragger.oldTop = dragger.top;
 
-      autoCropDragger.oldLeft = dragger.oldLeft = dragger.left;
-      autoCropDragger.oldTop = dragger.oldTop = dragger.top;
-
-      this.autoCropDragger = autoCropDragger;
-      this.defaultDragger = $.extend({}, dragger);
-      this.dragger = dragger;
+      this.defaultDragger = dragger;
+      this.dragger = $.extend({}, dragger);
     },
 
     renderDragger: function () {
@@ -684,7 +681,7 @@
       }
 
       if (data === NULL || $.isEmptyObject(data)) {
-        dragger = $.extend({}, this.autoCropDragger);
+        dragger = $.extend({}, this.defaultDragger);
       }
 
       if ($.isPlainObject(data) && !$.isEmptyObject(data)) {
