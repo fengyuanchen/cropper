ec294f7e7e88f12d3dbd5ed807cf1a8b0890bae4
Fengyuan Chen <cfy.net@gmail.com>
2015-02-09 11:07:16 +0800

    Update docs

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,11 +1,17 @@
-$(function() {
+$(function () {
 
-  var console = window.console || {log: function () {}},
+  "use strict";
+
+  var console = window.console || { log: function () {} },
       $alert = $(".docs-alert"),
       $message = $alert.find(".message"),
       showMessage = function (message, type) {
         $message.text(message);
-        type && $message.addClass(type);
+
+        if (type) {
+          $message.addClass(type);
+        }
+
         $alert.fadeIn();
 
         setTimeout(function () {
@@ -16,13 +22,12 @@ $(function() {
   // Overview
   // -------------------------------------------------------------------------
 
-  (function() {
-    var $image = $(".img-container img"),
+  (function () {
+    var $image = $(".img-container > img"),
         $dataX = $("#dataX"),
         $dataY = $("#dataY"),
         $dataHeight = $("#dataHeight"),
         $dataWidth = $("#dataWidth"),
-        $dataRotate = $("#dataRotate"),
         options = {
           aspectRatio: 16 / 9,
           data: {
@@ -32,20 +37,19 @@ $(function() {
             height: 360
           },
           preview: ".img-preview",
-          done: function(data) {
+          done: function (data) {
             $dataX.val(Math.round(data.x));
             $dataY.val(Math.round(data.y));
             $dataHeight.val(Math.round(data.height));
             $dataWidth.val(Math.round(data.width));
-            $dataRotate.val(data.rotate);
           }
         };
 
     $image.cropper(options).on({
-      "build.cropper": function(e) {
+      "build.cropper": function (e) {
         console.log(e.type);
       },
-      "built.cropper": function(e) {
+      "built.cropper": function (e) {
         console.log(e.type);
       }
     });
@@ -58,55 +62,55 @@ $(function() {
       }
     });
 
-    var $inputImage = $("#inputImage");
+    var $inputImage = $("#inputImage"),
+        blobURL;
 
-    if (window.FileReader) {
-      $inputImage.change(function() {
-        var fileReader = new FileReader(),
-            files = this.files,
+    if (window.URL) {
+      $inputImage.change(function () {
+        var files = this.files,
             file;
 
-        if (!files.length) {
-          return;
-        }
+        if (files && files.length) {
+          file = files[0];
 
-        file = files[0];
+          if (/^image\/\w+$/.test(file.type)) {
+            if (blobURL) {
+              URL.revokeObjectURL(blobURL); // Revoke the old one
+            }
 
-        if (/^image\/\w+$/.test(file.type)) {
-          fileReader.readAsDataURL(file);
-          fileReader.onload = function () {
-            $image.cropper("reset", true).cropper("replace", this.result);
+            blobURL = URL.createObjectURL(file);
+            $image.cropper("reset", true).cropper("replace", blobURL);
             $inputImage.val("");
-          };
-        } else {
-          showMessage("Please choose an image file.");
+          } else {
+            showMessage("Please choose an image file.");
+          }
         }
       });
     } else {
       $inputImage.parent().remove();
     }
 
-    $("#download").click(function() {
+    $("#download").click(function () {
       window.open($image.cropper("getDataURL"));
     });
 
     var $zoomWith = $("#zoomWith");
 
-    $("#zoom").click(function() {
+    $("#zoom").click(function () {
       $image.cropper("zoom", $zoomWith.val());
     });
 
 
     var $rotateWith = $("#rotateWith");
 
-    $("#rotate").click(function() {
+    $("#rotate").click(function () {
       $image.cropper("rotate", $rotateWith.val());
     });
 
 
     var $getDataInto = $("#getDataInto");
 
-    $("#getData").click(function() {
+    $("#getData").click(function () {
       var data = $image.cropper("getData"),
           val = "";
 
@@ -125,7 +129,7 @@ $(function() {
         $setDataWidth = $("#setDataWidth"),
         $setDataHeight = $("#setDataHeight");
 
-    $("#setData").click(function() {
+    $("#setData").click(function () {
       var data = {
             x: $setDataX.val(),
             y: $setDataY.val(),
@@ -139,21 +143,21 @@ $(function() {
 
     var $setAspectRatioWith = $("#setAspectRatioWith");
 
-    $("#setAspectRatio").click(function() {
+    $("#setAspectRatio").click(function () {
       $image.cropper("setAspectRatio", $setAspectRatioWith.val());
     });
 
 
     var $replaceWith = $("#replaceWith");
 
-    $("#replace").click(function() {
+    $("#replace").click(function () {
       $image.cropper("replace", $replaceWith.val());
     });
 
 
     var $getImageDataInto = $("#getImageDataInto");
 
-    $("#getImageData").click(function() {
+    $("#getImageData").click(function () {
       var data = $image.cropper("getImageData"),
           val = "";
 
@@ -170,21 +174,21 @@ $(function() {
     var $dataURLInto = $("#dataURLInto"),
         $dataURLView = $("#dataURLView");
 
-    $("#getDataURL").click(function() {
+    $("#getDataURL").click(function () {
       var dataURL = $image.cropper("getDataURL");
 
       $dataURLInto.text(dataURL);
       $dataURLView.html('<img src="' + dataURL + '">');
     });
 
-    $("#getDataURL2").click(function() {
+    $("#getDataURL2").click(function () {
       var dataURL = $image.cropper("getDataURL", "image/jpeg");
 
       $dataURLInto.text(dataURL);
       $dataURLView.html('<img src="' + dataURL + '">');
     });
 
-    $("#getDataURL3").click(function() {
+    $("#getDataURL3").click(function () {
       var dataURL = $image.cropper("getDataURL", {
             width: 160,
             height: 90
@@ -194,7 +198,7 @@ $(function() {
       $dataURLView.html('<img src="' + dataURL + '">');
     });
 
-    $("#getDataURL4").click(function() {
+    $("#getDataURL4").click(function () {
       var dataURL = $image.cropper("getDataURL", {
             width: 320,
             height: 180
@@ -204,7 +208,7 @@ $(function() {
       $dataURLView.html('<img src="' + dataURL + '">');
     });
 
-    $(".docs-options :radio").on("change", function (e) {
+    $(".docs-options :radio").on("change", function () {
       var $this = $(this);
 
       if ($this.is(":checked")) {
@@ -219,13 +223,13 @@ $(function() {
   // Sidebar
   // -------------------------------------------------------------------------
 
-  (function() {
+  (function () {
     var $sidebar = $(".docs-sidebar"),
-        offset = $sidebar.offset();
-        offsetTop = offset.top;
+        offset = $sidebar.offset(),
+        offsetTop = offset.top,
         mainHeight = $sidebar.parents(".row").height() - $sidebar.height();
 
-    $(window).bind("scroll", function() {
+    $(window).bind("scroll", function () {
       var st = $(this).scrollTop();
 
       if (st > offsetTop && (st - offsetTop) < mainHeight) {
@@ -258,16 +262,17 @@ $(function() {
   var $image = $(".bootstrap-modal-cropper > img"),
       originalData = {};
 
-  $("#bootstrap-modal").on("shown.bs.modal", function() {
+  $("#bootstrap-modal").on("shown.bs.modal", function () {
     $image.cropper({
       multiple: true,
       data: originalData,
-      done: function(data) {
+      done: function (data) {
         console.log(data);
       }
     });
-  }).on("hidden.bs.modal", function() {
+  }).on("hidden.bs.modal", function () {
     originalData = $image.cropper("getData"); // Saves the data on hide
     $image.cropper("destroy");
   });
+
 });
