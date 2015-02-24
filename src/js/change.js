2e98d63a5bab03f24e1ec7eb14997b032d75f144
Fengyuan Chen <cfy.net@gmail.com>
2015-02-24 12:44:21 +0800

    Fix typo

--- src/js/change.js
+++ src/js/change.js
@@ -310,7 +310,7 @@
       case 'zoom':
         this.zoom(function (x1, y1, x2, y2) {
           var z1 = sqrt(x1 * x1 + y1 * y1),
-              z2 = sqrt(x1 * x1 + y1 * y1);
+              z2 = sqrt(x2 * x2 + y2 * y2);
 
           return (z2 - z1) / z1;
         }(
