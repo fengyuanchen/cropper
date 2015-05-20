6c889513ea53144ca93a7ce7060e1339f2f430f1
Mohammad Younes <myounes@gmail.com>
2015-05-20 18:08:46 +0300

    Flip west/east

--- src/js/change.js
+++ src/js/change.js
@@ -34,7 +34,7 @@
       range.Y = range.x / aspectRatio;
     }
 
-    switch (dragType) {
+    switch (mapDragType(dragType)) {
       // Move cropBox
       case 'all':
         left += range.x;
@@ -56,7 +56,7 @@
         }
 
         if (width < 0) {
-          dragType = 'w';
+          dragType = mapDragType('w');
           width = 0;
         }
 
@@ -98,7 +98,7 @@
         }
 
         if (width < 0) {
-          dragType = 'e';
+          dragType = mapDragType('e');
           width = 0;
         }
 
@@ -157,14 +157,14 @@
         }
 
         if (width < 0 && height < 0) {
-          dragType = 'sw';
+          dragType = mapDragType('sw');
           height = 0;
           width = 0;
         } else if (width < 0) {
-          dragType = 'nw';
+          dragType = mapDragType('nw');
           width = 0;
         } else if (height < 0) {
-          dragType = 'se';
+          dragType = mapDragType('se');
           height = 0;
         }
 
@@ -206,14 +206,14 @@
         }
 
         if (width < 0 && height < 0) {
-          dragType = 'se';
+          dragType = mapDragType('se');
           height = 0;
           width = 0;
         } else if (width < 0) {
-          dragType = 'ne';
+          dragType = mapDragType('ne');
           width = 0;
         } else if (height < 0) {
-          dragType = 'sw';
+          dragType = mapDragType('sw');
           height = 0;
         }
 
@@ -252,14 +252,14 @@
         }
 
         if (width < 0 && height < 0) {
-          dragType = 'ne';
+          dragType = mapDragType('ne');
           height = 0;
           width = 0;
         } else if (width < 0) {
-          dragType = 'se';
+          dragType = mapDragType('se');
           width = 0;
         } else if (height < 0) {
-          dragType = 'nw';
+          dragType = mapDragType('nw');
           height = 0;
         }
 
@@ -295,14 +295,14 @@
         }
 
         if (width < 0 && height < 0) {
-          dragType = 'nw';
+          dragType = mapDragType('nw');
           height = 0;
           width = 0;
         } else if (width < 0) {
-          dragType = 'sw';
+          dragType = mapDragType('sw');
           width = 0;
         } else if (height < 0) {
-          dragType = 'ne';
+          dragType = mapDragType('ne');
           height = 0;
         }
 
@@ -346,17 +346,17 @@
 
           if (range.x > 0) {
             if (range.y > 0) {
-              dragType = 'se';
+              dragType = mapDragType('se');
             } else {
-              dragType = 'ne';
+              dragType = mapDragType('ne');
               top -= height;
             }
           } else {
             if (range.y > 0) {
-              dragType = 'sw';
+              dragType = mapDragType('sw');
               left -= width;
             } else {
-              dragType = 'nw';
+              dragType = mapDragType('nw');
               left -= width;
               top -= height;
             }
