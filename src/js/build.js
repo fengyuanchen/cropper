b4b6229df787558664bb2e0eccc2e9b5d5670da6
Fengyuan Chen <cfy.org@gmail.com>
2015-10-15 19:28:18 +0800

    The `aspectRatio` should be a positive number

--- src/js/build.js
+++ src/js/build.js
@@ -37,7 +37,7 @@
       this.bind();
 
       // Format aspect ratio (0 -> NaN)
-      options.aspectRatio = num(options.aspectRatio) || NaN;
+      options.aspectRatio = max(0, options.aspectRatio) || NaN;
 
       if (options.autoCrop) {
         this.cropped = true;
