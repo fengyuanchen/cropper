27b0f02c158eb7a9e09b5addab056d3497027841
Fengyuan Chen <cfy.org@gmail.com>
2015-12-06 12:54:01 +0800

    Update test suites of destroy method

--- test/methods/destroy.js
+++ test/methods/destroy.js
@@ -6,11 +6,9 @@ $(function () {
 
   QUnit.test('methods.destroy: before built', function (assert) {
     $image.cropper();
-    assert.ok($image.hasClass('cropper-hidden'));
     assert.ok(typeof $image.data('cropper') === 'object');
 
     $image.cropper('destroy');
-    assert.ok(!$image.hasClass('cropper-hidden'));
     assert.ok(typeof $image.data('cropper') === 'undefined');
 
     QUnit.test('methods.destroy: after built', function (assert) {
