653f01683e7c47745a0884df874325eed904e46b
Chen Fengyuan <chenfengyuan@outlook.com>
2016-08-30 12:10:50 +0800

    Fixed the bug of calling `ready` event twice

--- src/js/build.js
+++ src/js/build.js
@@ -79,7 +79,7 @@
       $this.one(EVENT_BUILT, options.built);
 
       // Trigger the built event asynchronously to keep `data('cropper')` is defined
-      setTimeout($.proxy(function () {
+      this.completing = setTimeout($.proxy(function () {
         this.trigger(EVENT_BUILT);
         this.trigger(EVENT_CROP, this.getData());
         this.isCompleted = true;
@@ -89,6 +89,10 @@
     unbuild: function () {
       if (!this.isBuilt) {
         return;
+      }
+
+      if (!this.isCompleted) {
+        clearTimeout(this.completing);
       }
 
       this.isBuilt = false;
