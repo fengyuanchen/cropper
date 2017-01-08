0570d45823cc0d29faee8678a3625ec6b8cf592a
Chen Fengyuan <chenfengyuan@outlook.com>
2017-01-08 15:25:30 +0800

    Simplify code

--- src/js/utilities.js
+++ src/js/utilities.js
@@ -236,12 +236,12 @@
       delete pointers2[pointerId];
 
       $.each(pointers2, function (pointerId2, pointer2) {
-        var x1 = Math.abs(pointer.startX - pointer2.startX);
-        var y1 = Math.abs(pointer.startY - pointer2.startY);
-        var x2 = Math.abs(pointer.endX - pointer2.endX);
-        var y2 = Math.abs(pointer.endY - pointer2.endY);
-        var z1 = Math.sqrt((x1 * x1) + (y1 * y1));
-        var z2 = Math.sqrt((x2 * x2) + (y2 * y2));
+        var x1 = abs(pointer.startX - pointer2.startX);
+        var y1 = abs(pointer.startY - pointer2.startY);
+        var x2 = abs(pointer.endX - pointer2.endX);
+        var y2 = abs(pointer.endY - pointer2.endY);
+        var z1 = sqrt((x1 * x1) + (y1 * y1));
+        var z2 = sqrt((x2 * x2) + (y2 * y2));
         var ratio = (z2 - z1) / z1;
 
         ratios.push(ratio);
@@ -249,7 +249,7 @@
     });
 
     ratios.sort(function (a, b) {
-      return Math.abs(a) < Math.abs(b);
+      return abs(a) < abs(b);
     });
 
     return ratios[0];
