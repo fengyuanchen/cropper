d2637a4805524c0ab9f1b1a76528321a718e786e
Fengyuan Chen <chenfengyuan@outlook.com>
2017-10-08 16:55:03 +0800

    build: add banner with plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,9 @@
 const babel = require('rollup-plugin-babel');
 const commonjs = require('rollup-plugin-commonjs');
 const nodeResolve = require('rollup-plugin-node-resolve');
+const pkg = require('./package');
+
+const now = new Date();
 
 module.exports = {
   input: 'src/js/index.js',
@@ -36,4 +39,14 @@ module.exports = {
       exclude: 'node_modules/**',
     }),
   ],
+  banner: `/*!
+ * Cropper v${pkg.version}
+ * https://github.com/${pkg.repository}
+ *
+ * Copyright (c) 2014-${now.getFullYear()} ${pkg.author.name}
+ * Released under the ${pkg.license} license
+ *
+ * Date: ${now.toISOString()}
+ */
+`,
 };
