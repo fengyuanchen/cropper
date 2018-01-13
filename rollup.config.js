d3e24208e8ebd1f2959dafaecb71de0a0ead4b11
Chen Fengyuan <chenfengyuan@outlook.com>
2018-01-13 17:50:18 +0800

    build: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,28 +4,45 @@ const nodeResolve = require('rollup-plugin-node-resolve');
 const pkg = require('./package');
 
 const now = new Date();
+const banner = `/*!
+ * Cropper v${pkg.version}
+ * https://github.com/${pkg.repository}
+ *
+ * Copyright (c) 2014-${now.getFullYear()} ${pkg.author.name}
+ * Released under the ${pkg.license} license
+ *
+ * Date: ${now.toISOString()}
+ */
+`;
 
 module.exports = {
+  // Export banner for PostCSS
+  banner,
   input: 'src/js/index.js',
   output: [
     {
+      banner,
       file: 'dist/cropper.js',
       format: 'umd',
+      name: 'Cropper',
     },
     {
+      banner,
       file: 'dist/cropper.common.js',
       format: 'cjs',
     },
     {
+      banner,
       file: 'dist/cropper.esm.js',
       format: 'es',
     },
     {
+      banner,
       file: 'docs/js/cropper.js',
       format: 'umd',
+      name: 'Cropper',
     },
   ],
-  name: 'Cropper',
   external: ['jquery'],
   globals: {
     jquery: 'jQuery',
@@ -35,16 +52,7 @@ module.exports = {
     commonjs(),
     babel({
       exclude: 'node_modules/**',
+      plugins: ['external-helpers'],
     }),
   ],
-  banner: `/*!
- * Cropper v${pkg.version}
- * https://github.com/${pkg.repository}
- *
- * Copyright (c) 2014-${now.getFullYear()} ${pkg.author.name}
- * Released under the ${pkg.license} license
- *
- * Date: ${now.toISOString()}
- */
-`,
 };
