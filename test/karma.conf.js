59c29eac64e2a305c7a60055328a25b34097c4d0
Chen Fengyuan <chenfengyuan@outlook.com>
2018-09-03 20:34:31 +0800

    build: simplify config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -33,8 +33,6 @@ module.exports = (config) => {
       plugins: rollupConfig.plugins,
       output: {
         format: 'iife',
-        name: 'Cropper',
-        sourcemap: 'inline',
       },
     },
     singleRun: true,
